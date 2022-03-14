fbed57bad5546927fee5c24bb4657174a5c5f15a
Gee Bartlett <geeus81@gmail.com>
2022-03-14 17:54:33 +0000

    More notes and ideas

--- src/config.py
+++ src/config.py
@@ -2,3 +2,14 @@
 #contains settings for generating tech tree
 
 DEBUG  = 1
+__version__ = '0.0.1'
+
+
+#CATIDs
+resourceID = 0 
+consumableID = 0
+itemID = 0
+levelID = 0
+
+#coinlist
+pathToCoinList = '../config/coinlists.json'
